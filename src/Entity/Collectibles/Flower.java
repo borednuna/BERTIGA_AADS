1246fb2807acf775790991a586c58ad497155d8e
borednuna <hanunshaka02@gmail.com>
2022-12-19 22:42:08 +0700

    created flower class

---
+++ src/Entity/Collectibles/Flower.java
@@ -0,0 +1,57 @@
+package Entity.Collectibles;
+
+import java.awt.Graphics;
+
+import Entity.Playable.Playable;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+import javax.imageio.ImageIO;
+
+import Main.GamePanel;
+import Map.Map;
+import Entity.Playable.*;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Flower extends Collectibles {
+    Playable mc;
+
+    public Flower(Playable mc, int x, int y){
+        this.collected = false;
+        this.mc = mc;
+        this.x = x;
+        this.y = y;
+        this.x_range = 50;
+
+        try {
+            icon = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Entity/Collectibles/flower.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.drawImage(icon, x, y, null);
+    }
+
+    @Override
+    public void update() {
+        if (!collected) {
+            if (isCatch(mc)) {
+                mc.addScore(100);
+                dispose();
+                collected = true;
+            }
+        }
+    }
+
+    @Override
+    public void dispose() {
+        icon = null;
+    }
+    
+}
